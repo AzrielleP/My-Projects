72146ccb0fa18c44e13d8650698c79480da86ed0
Aze <azrielle101@gmail.com>
2020-07-15 23:21:13 +0800

    Polished the website

--- index.js
+++ index.js
@@ -11,4 +11,23 @@ const toggleMenu = (() => {
 
 })();
 
+const styleNav = (() => {
+    const links = document.querySelector('.page-links');
+    
+    const resetLinkColor = () => {
+        const childLinks = links.getElementsByTagName('a');
+        for (let i = 0; i < childLinks.length; i += 1) {
+            childLinks[i].style.border = 'none';
+        }
+    }
+
+    links.addEventListener('click', (event) => {
+        if (event.target !== event.currentTarget) {
+            resetLinkColor();
+            event.target.style.borderBottom = '4px solid #f7ce3e';
+        }
+        event.stopPropagation();
+    })
+})();
+
 
